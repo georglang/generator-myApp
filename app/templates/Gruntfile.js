e190e5eeeaa19f3650c57ba0f0ca899424ab9dc1
Georg Lang <you@example.com>
2014-03-23 20:09:54 +0100

    added index.html and Gruntfile.js

---
+++ app/templates/Gruntfile.js
@@ -0,0 +1,62 @@
+'use strict';
+
+var LIVERELOAD_PORT = 35729;
+var lrSnippet = require('connect-livereload')({port: LIVERELOAD_PORT});
+var mountFolder = function (connect, dir) {
+  return connect.static(require('path').resolve(dir));
+};
+
+module.exports = function (grunt) {
+// load all grunt tasks
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+  grunt.initConfig({
+    watch: {
+      options: {
+        nospawn: true,
+        livereload: LIVERELOAD_PORT
+      },
+      livereload: {
+        files: [
+          'index.html',
+          'css/style.css'
+        ]
+      }
+    },
+    connect: {
+      options: {
+        port: 9000,
+        hostname: 'localhost'
+      },
+      livereload: {
+        options: {
+          middleware: function (connect) {
+            return [
+              lrSnippet,
+              mountFolder(connect, '.')
+            ];
+          }
+        }
+      }
+    },
+    open: {
+      server: {
+        path: 'http://localhost:<%%= connect.options.port %>'
+      }
+    },
+    sass: {
+      dist: {
+        options: {
+          style: 'expanded'
+        },
+        files: {
+          'css/style.css': 'css/style.sass',
+          'css/reset.css': 'css/reset.sass',
+          'css/bootstrap-sass-official/bootstrap.css': 'bower_components/bootstrap-sass-official/vendor/assets/stylesheets/bootstrap.scss'
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('server', ['sass', 'connect:livereload', 'open', 'watch']);
+};
